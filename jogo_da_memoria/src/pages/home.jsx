37ea71d3176084305d42076a5652ba8f3977d3f4
Augusto <augustowieles@gmail.com>
2024-04-25 00:37:42 -0300

    home

--- jogo_da_memoria/src/pages/home.jsx
+++ jogo_da_memoria/src/pages/home.jsx
@@ -1,11 +1,16 @@
 import { View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Image } from "react-native";
 import React from "react";
 
 export default function Home(){
    return(
-      <View className="flex-1 items-center justify-center bg-green-950">
-         <View>
+      <View className="flex-1 bg-green-900">
+         <View style={{flex: 0.5}} className="flex items-center justify-center gap-2">
+            <Image source={require("../../assets/logo.png")} style={{width: 300, height: 300, resizeMode: 'contain'}}/>
+            <Text className=" text-xl text-white font-bold">Jogo da Memória Temático de Futebol</Text>            
+         </View>
+         <View style={{flex: 0.5}}> 
          </View>
          <StatusBar style="auto" />
       </View>
