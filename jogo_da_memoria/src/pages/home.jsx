3cbfb55a2c4c1626a4045fcc00ae105bb8f61006
Augusto <augustowieles@gmail.com>
2024-04-26 00:22:26 -0300

    aa

--- jogo_da_memoria/src/pages/home.jsx
+++ jogo_da_memoria/src/pages/home.jsx
@@ -1,18 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, KeyboardAvoidingView, Touchable, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Image } from "react-native";
 import React from "react";
 
 export default function Home(){
    return(
+      <KeyboardAvoidingView className="flex-1 bg-green-900" behavior="padding" enabled>
       <View className="flex-1 bg-green-900">
-         <View style={{flex: 0.5}} className="flex items-center justify-center gap-2">
-            <Image source={require("../../assets/logo.png")} style={{width: 300, height: 300, resizeMode: 'contain'}}/>
-            <Text className=" text-xl text-white font-bold">Jogo da Memória Temático de Futebol</Text>            
-         </View>
-         <View style={{flex: 0.5}}> 
-         </View>
-         <StatusBar style="auto" />
+         <View style={{flex: 1}} className="flex items-center justify-center gap-12">
+            <Image source={require("../../assets/logo.png")} style={{width: 300, height: 290}}/>
+            <View className="w-full px-14">
+               <TextInput className="bg-slate-50 px-3 rounded-lg h-12 text-xl" placeholder="Nome do Jogador"/>
+               <TouchableOpacity className="items-center justify-center bg-green-600 mt-6 rounded-lg h-12 border-white">
+                  <Text className="text-xl text-white font-bold">
+                     Iniciar Jogo
+                  </Text>
+               </TouchableOpacity>
+               <TouchableOpacity className="items-center justify-center bg-blue-500 mt-6 rounded-lg h-12">
+                  <Text className="text-xl text-white font-bold">
+                     Ranking
+                  </Text>
+               </TouchableOpacity>
+            </View>            
+         </View>         
+         <StatusBar style="light" />
       </View>
+      </KeyboardAvoidingView>
    )
 }
